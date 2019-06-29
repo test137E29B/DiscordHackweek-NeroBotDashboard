d7b7e4c171b58d561720b018d4a0c65f00be5300
Nameless <badxnameless@gmail.com>
2019-06-29 06:57:54 +0100

    Update data for live bot

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,16 +1,16 @@
 export const BASE_API_URL =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:80"
-    : "http://localhost:80";
+    ? "https://nerobot.aquilamc.net"
+    : "https://nerobot.aquilamc.net";
 
 export const BASE_WEB_URL =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : "http://localhost:3000";
+    ? "https://nero.aquilamc.net"
+    : "https://nero.aquilamc.net";
 
 export const LOGIN_URL = BASE_WEB_URL + "/login";
 
 export const CLIENT_ID =
   process.env.NODE_ENV === "development"
-    ? "592761728991887367"
-    : "592761728991887367";
+    ? "592761856075235339"
+    : "592761856075235339";
